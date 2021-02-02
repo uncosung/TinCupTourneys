c1fe91c044b70abe3f91257d50652119a9b63a3a
uncosung <eric.k.sung@gmail.com>
2021-02-02 15:34:36 -0800

    Skeleton

---
+++ client/src/index.jsx
@@ -0,0 +1,4 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<div>Hello World</div>, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const path = require('path');
+const BUILD_DIR = path.resolve(__dirname, 'client/dist');
+const APP_DIR = path.resolve(__dirname, 'client/src');
+const config = {
+  entry: APP_DIR + '/index.jsx',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  resolve: {
+    test: /\.css$/,
+      include: /node_modules/,
+      loaders: ['style-loader', 'css-loader', 'postcss-loader'],
+  }
+};
+module.exports = config;
